0eb50f297b4bafac6637ad168cced74f103218bb
geshyrihu <geshyrihu@PC-MARQUEZ>
2026-07-10 08:06:40 -0600

    renombre de interfacs

--- src/app/apps/admin.luxuryapp/seguridad-permisos/approval-rules/models/approval-rules.dto.ts
+++ src/app/apps/admin.luxuryapp/seguridad-permisos/approval-rules/models/approval-rules.dto.ts
@@ -1,5 +1,7 @@
+import { ApprovalScope } from "./approval-rules.enum";
+
 export interface ApprovalRuleDto {
-    approverRole: string;
-    targetRole: string;
-    approvalScope: ApprovalScope;
+  approverRole: string;
+  targetRole: string;
+  approvalScope: ApprovalScope;
 }
