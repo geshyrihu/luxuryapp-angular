6e33f904590e646d557860185fab8976f405601d
geshyrihu <geshyrihu@PC-MARQUEZ>
2026-08-15 12:06:59 -0600

    limpieza fase 2

--- src/app/apps/admin.luxuryapp/herramientas-dev/catalog-component-ui/interfaces/print-config.model.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('print-config.model', () => {
-  it('should exist', () => {
-    expect(true).toBe(true);
-  });
-});
--- src/app/apps/admin.luxuryapp/seguridad-permisos/customer-modul/interfaces/customer-modul.dto.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('customer-modul.dto', () => {
-  it('should exist', () => {
-    expect(true).toBe(true);
-  });
-});
